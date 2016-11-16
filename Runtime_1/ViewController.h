1b6d70a263a41cb1dd49dcc812852ff751f56ccd
徐徐如征 <zheng_anjing@163.com>
2016-11-16 10:45:53 +0800

    Initial Commit

---
+++ Runtime_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime_1
+//
+//  Created by 三米 on 16/11/16.
+//  Copyright © 2016年 三米. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Runtime_1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Runtime_1
+//
+//  Created by 三米 on 16/11/16.
+//  Copyright © 2016年 三米. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
